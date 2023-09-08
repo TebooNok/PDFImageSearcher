a10aa0d9341289146f08ac445d02d8729e7f8cbc
TebooNok <450015317@qq.com>
2023-09-09 01:58:02 +0800

    Support Vector Figure extract and query Remove duplicate image

--- pdfImage.py
+++ pdfImage.py
@@ -68,15 +68,19 @@ def load_pdf(file, dpi=300, skip_page_front=0, skip_page_back=1, skip_block=5, l
     # if os.path.exists(path_name):
     #     shutil.rmtree(path_name)
     # os.mkdir(path_name)
+    #
+    # temp_image_dir = path_name
     temp_image_dir = tempfile.mkdtemp(prefix='images_')
 
     for page in pages[int(skip_page_front):-int(skip_page_back)]:  # skip final page
 
+        # part1: get image with description in png-pdf
         p1dict = page.get_text('dict')
         blocks = p1dict['blocks']
         page_pix = page.get_pixmap(matrix=matrix, dpi=dpi)
         page_im = Image.frombytes("RGB", (page_pix.width, page_pix.height), page_pix.samples)
 
+        saved = []  # need to remove if inner a svg image
         for i, block in enumerate(blocks[int(skip_block):]):  # head and tail of pages should be ignore
             if 'image' in block:
                 # try:
@@ -97,10 +101,74 @@ def load_pdf(file, dpi=300, skip_page_front=0, skip_page_back=1, skip_block=5, l
                     title = get_title_of_image(blocks[skip_block:], i, lang)
                     # print(text_content[:30])
                     # print(title)
-                    with open(f'{file_name}.txt', 'w') as text_file:
+                    with open(f'{file_name}.txt', 'w', encoding='utf-8') as text_file:
                         text_file.write(title + '\n' + text_content.replace('\n', ' '))
+
+                    saved.append((file_name, [int(i * scale) for i in bbox]))
                 # except:
                 #     pass
+
+        # part2: get image with description in svg-pdf
+        svg = page.get_svg_image(matrix=fitz.Identity)
+        image_clips, svg_blocks = parse_page_svg(svg, page.number)
+        for clip in image_clips:
+
+            transform = []
+            for item in clip[0]:
+                # print(item, type(item))
+                if item[0] == '.':
+                    transform.append(float('0' + item))
+                elif item[0] == '-':
+                    transform.append(float('-0' + item[1:]))
+                else:
+                    transform.append(float(item))
+            d = clip[1]
+            page_id = clip[2]
+            block_id = clip[3]
+            matches = re.findall(r'H(\d+\.?\d*)V(\d+\.?\d*)', d)
+            float_values = [float(value) for value in matches[0]]
+            box_width = float_values[0]
+            box_height = float_values[1]
+            width_scale = transform[0]
+            height_scale = transform[3]
+            width_move = transform[4]
+            height_move = transform[5]
+            x1 = width_move * scale
+            y1 = height_move * scale
+            # x1=347*scale
+            # y1=587*scale
+            x2 = x1 + box_width * width_scale * scale
+            y2 = y1 + box_height * height_scale * scale
+
+            if y1 > y2:
+                y1, y2 = y2, y1
+
+            # print(x1, y1, x2, y2)
+            # 3. 截取并保存图像
+
+            # check images in saved, if in or similar, delete it from file system
+            for i, (file_name, bbox) in enumerate(saved):
+                if (abs(bbox[0] - x1) < 10\
+                        and abs(bbox[1] - y1) < 10\
+                        and abs(bbox[2] - x2) < 10\
+                        and abs(bbox[3] - y2) < 10) or \
+                        (bbox[0]>x1-10 and bbox[1]>y1-10 and bbox[2]<x2+10 and bbox[3]<y2+10):
+                    os.remove(file_name + '.png')
+                    os.remove(file_name + '.txt')
+                    saved.pop(i)
+                    break
+
+            cropped_img = page_im.crop((int(x1), int(y1), int(x2), int(y2)))
+            file_name = temp_image_dir + f'/svg_image_{page.number}_{block_id}'
+            image_name = file_name + '.png'
+            cropped_img.save(image_name)
+
+            # search title and text
+            text_content = get_svg_text_around_image(svg_blocks, block_id, lang)
+            title = get_svg_title_around_image(svg_blocks, block_id, lang)
+            with open(f'{file_name}.txt', 'w', encoding='utf-8') as text_file:
+                text_file.write(title + '\n' + text_content.replace('\n', ' '))
+
     return temp_image_dir
 
 
@@ -129,7 +197,7 @@ def build_index(file, tmp_dir, lang='CN'):
     for file in os.listdir(tmp_dir):
         if file.endswith('.txt'):
             file_path = os.path.join(tmp_dir, file)
-            with open(file_path, 'r') as f:
+            with open(file_path, 'r', encoding='utf-8') as f:
                 content = f.read()
             writer.add_document(file_name=file[:-4], content=content)
             # print('==========')
@@ -174,9 +242,11 @@ def return_image(file, results_list, tmp_dir):
 
 
 # file = 'CA-IS372x-datasheet_cn.pdf'
+# file = 'CA-IS3086 datasheet_cn.pdf'
 # temp_image_dir = load_pdf(file, lang='CN')
 # ix, temp_index_dir = build_index(file, temp_image_dir)
-# results_list = search(ix, '波形', lang='CN', k=10)
+# results_list = search(ix, """title: 图 7-1输入等效电路输入等效电路
+# CA-IS3086W, CA-IS3086WE 上海川土微电子有限公司上海川土微电子有限公司 输入等效电路输入等效电路 图 7-1输入等效电路输入等效电路 8 功能描述功能描述 简述简述 CA-IS3086 是用于 RS-485通信的全双工收发器，包含一个驱动器和接收器。具有失效安全，过流保护和过热保护""", lang='CN', k=10)
 # ret_img = return_image(file, results_list, temp_image_dir)
 # print('title: ' + ret_img[0])
 # ret_img[1].show()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+
 def get_adjacent_lines(blocks, block_index):
     """
     Returns two lists: the lines of text before and after the block at block_index.
@@ -95,6 +97,163 @@ def get_title_of_image(blocks, image_index, lang='CN'):
     # Search for a title in the lines after the image
     for line in after_lines:
         if lang == 'CN' and '图 ' in line:
+            return f"title: {line}"
+        elif 'figure' in line.lower():
+            return f"title: {line}"
+
+    return title if title else "title: Not Found"
+
+
+def transform_to_array(trans):
+    trans = trans.replace('matrix(', '').replace(')', '').split(',')
+    arr = []
+    # print(trans)
+    for item in trans:
+        # print(item, type(item))
+        if item[0] == '.':
+            arr.append(float('0' + item))
+        elif item[0] == '-':
+            arr.append(float('-0'+item[1:]))
+        else:
+            arr.append(float(item))
+    return arr
+
+
+def parse_page_svg(svg, page_id):
+    # 解析SVG内容
+    root = ET.fromstring(svg)
+
+    # 获取页面大小
+    width = int(root.get('width').replace('pt', ''))
+    height = int(root.get('height').replace('pt', ''))
+
+    # 存储clipPaths
+    clips = {}
+    for clip in root.findall('.//{http://www.w3.org/2000/svg}clipPath'):
+        clips[clip.get('id')] = clip
+
+    # 获取SVG下的第一个g标签
+    main_g = root.find('{http://www.w3.org/2000/svg}g')
+
+    page_size = f'H{width}V{height}'
+    gs = main_g.findall('{http://www.w3.org/2000/svg}g')
+
+    block_id = 0
+    img_clips = []
+    blocks = []
+    cache = ""
+    vertical = None
+    horizon = None
+    # 遍历主g标签下的所有子g标签
+    for g in main_g.findall('{http://www.w3.org/2000/svg}g'):
+
+        # 检查第一个子标签是否为"use"标签并且是否有"data-text"属性
+        first_child = list(g)[0] if g else None
+        if first_child is not None and first_child.tag == "{http://www.w3.org/2000/svg}use" and 'data-text' in first_child.attrib:
+            # get all use tags that contains data-text attribute in g tag and print them
+            for u in g.findall('{http://www.w3.org/2000/svg}use'):
+                if 'data-text' in u.attrib:
+                    text_vertical = transform_to_array(u.get('transform'))[5]
+                    text_horizon = transform_to_array(u.get('transform'))[4]
+                    if vertical is None or abs(text_vertical - vertical) > 10:
+                        vertical = text_vertical
+                        cache = cache.strip()
+                        if cache != "":
+                            blocks.append(cache)
+                            cache = u.get('data-text')
+                            block_id += 1
+                    else:
+                        # horizon should change
+                        if horizon is None or abs(text_horizon - horizon) > 1:
+                            horizon = text_horizon
+                            cache += u.get('data-text')
+            continue
+
+        clip_path = g.get('clip-path')
+        if clip_path and '#clip_' in clip_path:
+            clip_id = clip_path.split("#")[1].replace(')', '')
+            if clip_id in clips:
+                path = clips[clip_id].find('.//{http://www.w3.org/2000/svg}path')
+                transform = path.get('transform')
+                if not transform:
+                    continue
+                transform = transform.replace('matrix(', '').replace(')', '')
+                d = path.get('d')
+                trans_height = int(float(transform.split(',')[5]))
+                if not (page_size in d or (transform and trans_height == height)):
+                    # print(page_size in d)
+                    # print(transform and trans_height == height)
+                    # print(f"From Transform: {transform}, D: {d}", page_size, trans_height, height)
+                    # print(f"From Transform: {transform}, D: {d} in page {page_id}")
+                    img_clips.append((transform.split(','), d, page_id, block_id))
+                    blocks.append(f'image_{block_id}')
+                    block_id += 1
+                else:
+                    for sub_g in g.findall('.//{http://www.w3.org/2000/svg}g'):
+                        sub_clip_path = sub_g.get('clip-path')
+                        if sub_clip_path and '#clip_' in sub_clip_path:
+                            sub_clip_id = sub_clip_path.split("#")[1].replace(')', '')
+                            if sub_clip_id in clips:
+                                sub_path = clips[sub_clip_id].find('.//{http://www.w3.org/2000/svg}path')
+                                sub_d = sub_path.get('d')
+                                sub_transform = sub_path.get('transform')
+                                sub_transform = sub_transform.replace('matrix(', '').replace(')', '')
+                                subtrans_height = int(float(sub_transform.split(',')[5]))
+                                if not (page_size in sub_d or (sub_transform and subtrans_height == height)):
+                                    # print(f"From sub Transform: |{sub_transform}|, D: {sub_d} in page {page_id}")
+                                    img_clips.append((sub_transform.split(','), sub_d, page_id, block_id))
+                                    blocks.append(f'image_{block_id}')
+                                    block_id += 1
+                                    break
+    return img_clips, blocks
+
+
+def get_svg_text_around_image(blocks, block_id, lang='CN', word_count=50):
+    text_content = ""
+    counter = word_count
+
+    # Process lines before the image
+    for line in reversed(blocks[:block_id]):
+        text_content = line + '\n' + text_content
+        if lang == 'CN':
+            counter -= len(line)
+        else:
+            counter -= len(line.split(' '))
+        if counter <= 0:
+            break
+
+    # Reset the word counter for lines after the image
+    counter = word_count
+
+    # Process lines after the image
+    for line in blocks[block_id+1:]:
+        text_content += line + '\n'
+        if lang == 'CN':
+            counter -= len(line)
+        else:
+            counter -= len(line.split(' '))
+        if counter <= 0:
+            break
+
+    return text_content.strip()
+
+
+def get_svg_title_around_image(blocks, block_id, lang='CN'):
+    # Search for a title in the lines before the image
+    title = None
+    for line in reversed(blocks[:block_id]):
+        if lang == 'CN' and '图' in line:
+            title = f"title: {line}"
+            break
+        elif 'figure' in line.lower():
+            title = f"title: {line}"
+            break
+
+    # Search for a title in the lines after the image
+    for line in blocks[block_id+1:]:
+        if lang == 'CN' and '图 ' in line:
+            return f"title: {line}"
+        elif lang == 'CN' and '图' in line:
             return f"title: {line}"
         elif 'figure' in line.lower():
             return f"title: {line}"
